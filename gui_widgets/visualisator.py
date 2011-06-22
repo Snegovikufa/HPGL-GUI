ebd911fb1a2abf3fc17e7dd030842da984d50a02
Snegovikufa <rustiksnegovik@gmail.com>
2011-06-22 10:09:38 +0600

    Another fix for PySide

--- gui_widgets/visualisator.py
+++ gui_widgets/visualisator.py
@@ -54,7 +54,6 @@ class MayaviQWidget(QtGui.QWidget):
         QtGui.QWidget.__init__(self, parent)
 
         layout = QtGui.QVBoxLayout(self)
-        layout.setMargin(0)
         layout.setSpacing(0)
 
         self.visualization = Visualisator()
